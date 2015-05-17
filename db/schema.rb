c8c31b639b6b224ebfba161266f224a66cfc08ec
Frizky Kramer <frizkykramer@gmail.com>
2015-05-18 00:47:29 +0200

    Removed description from todo lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517125828) do
+ActiveRecord::Schema.define(version: 20150517223828) do
 
   create_table "todo_items", force: :cascade do |t|
     t.integer  "todo_list_id"
@@ -25,9 +25,8 @@ ActiveRecord::Schema.define(version: 20150517125828) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
