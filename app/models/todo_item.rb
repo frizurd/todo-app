2b5386ff5f17302bd2e9516131c230ac102de63c
Frizky Kramer <frizkykramer@gmail.com>
2015-05-18 02:59:05 +0200

    Added images and removed default stylesheet

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -7,8 +7,15 @@ class TodoItem < ActiveRecord::Base
   scope :complete, -> { where("completed_at is not null") }
   scope :incomplete, -> { where(completed_at: nil) }
 
-
   def completed?
     !completed_at.blank?
+  end
+
+  def toggle_completion!
+    if completed?
+      update_attribute(:completed_at, nil)
+    else
+      update_attribute(:completed_at, Time.now)
+    end
   end
 end
